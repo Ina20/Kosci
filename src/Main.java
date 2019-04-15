573feff0a177d3076fddce33424128ee8a0a3d40
Ina20 <49551246+Ina20@users.noreply.github.com>
2019-04-15 15:27:45 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import sun.java2d.windows.GDIRenderer;
 
 import javax.swing.*;
 import java.awt.*;
@@ -26,64 +25,12 @@ public class Main {
         ImageIcon d5 = new ImageIcon("src/kostka5.png");
         ImageIcon d6 = new ImageIcon("src/kostka6.png");
 
-        GridBagLayout layout = new GridBagLayout();
-        GridBagConstraints c = null;
-        Insets insets = new Insets(0,0,0,0);
-
-        JFrame frame = new JFrame("Kosci");
-        frame.getContentPane().setLayout(layout);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(800,700));
-
+        Board board = new Board();
+        board.createBoard();
 
 //      Dlaczego sie nie commitujesz? xD
-        ImagePanel imp1 = new ImagePanel();
-        //imp1.setBackground(Color.black);
-        imp1.setPreferredSize(new Dimension(500, 600));
-        c = new GridBagConstraints(1, 0, 1, 1, 0.6, 0.8, GridBagConstraints.PAGE_START, GridBagConstraints.BOTH, insets, 0, 450);
-        frame.getContentPane().add(imp1, c);
-/*
-        ImagePanel imp2 = new ImagePanel();
-        imp2.setPreferredSize(new Dimension(100, 400));
-        frame.getContentPane().add(imp2);
 
-        ImagePanel2 imp3 = new ImagePanel2();
-        imp3.setPreferredSize(new Dimension(100, 400));
-        frame.getContentPane().add(imp3);
-
-        ImagePanel3 imp4 = new ImagePanel3();
-        imp4.setPreferredSize(new Dimension(100, 400));
-        frame.getContentPane().add(imp4);
-
-        ImagePanel4 imp5 = new ImagePanel4();
-        imp5.setPreferredSize(new Dimension(100, 400));
-        frame.getContentPane().add(imp5);
-*/
-
-        JButton button = new JButton("Roll");
-        button.setPreferredSize(new Dimension(200, 50));
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setPreferredSize(new Dimension(500, 100));
-        c = new GridBagConstraints(1, 1, 1, 1, 0.6, 0.2, GridBagConstraints.PAGE_END, GridBagConstraints.BOTH, insets, 0, 0);
-        frame.getContentPane().add(buttonPanel, c);
-        buttonPanel.add(button, new FlowLayout(FlowLayout.CENTER));
-
-
-        JTable table = new JTable(18,3);
-        table.setPreferredSize(new Dimension(300, 630));
-        for(int i=0; i<18; i++) {
-            table.setRowHeight(i, 35);
-        }
-
-        JPanel tablePanel = new JPanel();
-        tablePanel.setPreferredSize(new Dimension(300, 700));
-        c = new GridBagConstraints(0, 0, 1, 2, 0.4, 1, GridBagConstraints.FIRST_LINE_START, GridBagConstraints.BOTH, insets, 0, 0);
-        frame.getContentPane().add(tablePanel, c);
-        tablePanel.add(table);
-
-
-        button.addActionListener(new ActionListener() {
+        board.button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -187,16 +134,13 @@ public class Main {
                         dice5 = d6.getImage();
                         break;
                 }
-                imp1.setImage(dice1,dice2,dice3,dice4,dice5);
+                board.imp.setImage(dice1,dice2,dice3,dice4,dice5);
             }
         });
-
 
         //imp2.setImage(dice2);
         //imp3.setImage(dice3);
         //imp4.setImage(dice4);
         //imp5.setImage(dice5);
-        frame.pack();
-        frame.setVisible(true);
     }
 }

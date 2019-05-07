f5d018751ba13f6e012ec6c02988201b87be9780
panda020 <49525674+panda020@users.noreply.github.com>
2019-05-07 11:53:41 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.stream.Stream;
 
@@ -44,24 +46,24 @@ public class Main {
         z5 = new ImageIcon("src/zaznaczenie5.png");
         z6 = new ImageIcon("src/zaznaczenie6.png");
 
+        player1Name = JOptionPane.showInputDialog("Enter your name");
+        if(player1Name == null){
+            player1Name = "Gracz 1";
+        }
 
-            player2Name = JOptionPane.showInputDialog("Enter your name");
-            if(player2Name == null){
-                player2Name = "Gracz 2";
-            }
-
-
-        //tworzenie klienta
+        //tworzenie serwera
+        ServerSocket ss = null;
         Socket s = null;
         try {
-            s = new Socket("localhost",4999);
+            InetAddress address = InetAddress.getByName("172.20.10.2");
+            turn = true;
+          //  ss = new ServerSocket( 4999, 50, address );
+            ss = new ServerSocket( 4999 );
+            s = ss.accept();
             pr = new PrintWriter( s.getOutputStream());
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-
-        pr.println( player2Name );
-        pr.flush();
         InputStreamReader in = null;
         BufferedReader bf = null;
         String name;
@@ -69,7 +71,9 @@ public class Main {
             in = new InputStreamReader( s.getInputStream() );
             bf = new BufferedReader( in );
             name = bf.readLine();
-            player1Name = name;
+            player2Name = name;
+            pr.println( player1Name );
+            pr.flush();
         } catch (IOException e1) {
             e1.printStackTrace();
         }
@@ -147,7 +151,7 @@ public class Main {
                     GameLogic l = new GameLogic();
                     if (rolls > 0) {
                         rolls -= 1;
-                        board.text.setText("Tura gracza: " + "\n" + player2Name + "\nPozostało \nrzutów: " + rolls);
+                        board.text.setText("Tura gracza: " + "\n" + player1Name + "\nPozostało \nrzutów: " + rolls);
 
                         if (!click1) {
                             kostka1 = l.roll();
@@ -178,20 +182,18 @@ public class Main {
                         int tab[] = l.check( kostka1, kostka2, kostka3, kostka4, kostka5 );
 
                         for (int i = 0; i < 17; i++) {
-
                                 if (board.table.isRowSelected( i ) == false && selected[i] == 0) {
-                                    board.table.setValueAt( tab[i], i, 2 );
-                                    board.table.setValueAt(suma21,7,2);
-                                    board.table.setValueAt(suma22,15,2);
-                                    if(suma12 >= 63){
-                                        board.table.setValueAt("35", 6, 2);
+                                    board.table.setValueAt( tab[i], i, 1 );
+                                    board.table.setValueAt(suma11,7,1);
+                                    board.table.setValueAt(suma12,15,1);
+                                    if(suma11 >= 63){
+                                        board.table.setValueAt("35", 6, 1);
                                     }
-                                    board.table.setValueAt(razem1,16,2);                                }
-                                else{
+                                    board.table.setValueAt(razem,16,1);                                }
+                                else {
                                     selected[i] = 1;
                                 }
-
-                            }
+                        }
                         board.imp.setImage( dice1, zaz1 );
                         board.imp1.setImage( dice2, zaz2 );
                         board.imp2.setImage( dice3, zaz3 );
@@ -214,43 +216,40 @@ public class Main {
                 pr.flush();
                 click1 = false; click2 = false; click3 = false; click4 = false; click5 = false;
                 pr.flush();
-                gameLength += 1;
-                board.text.setText("Tura gracza: " + "\n" + player1Name + "\nPozostało \nrzutów: " + 3);
-                for (int i = 0; i < 17; i++) {
-                    board.table.setValueAt(suma21, 7, 2);
-                    board.table.setValueAt(suma22, 15, 2);
-                    if(suma12 >= 63){
-                        board.table.setValueAt("35", 6, 2);
-                    }
-                    razem1 = suma21 + suma22 + Integer.parseInt( String.valueOf( board.table.getValueAt( 6, 2 ) ));
-                    board.table.setValueAt(razem1,16,2);
-                    if (board.table.isRowSelected( i ) == true) {
-                        if (i < 7) {
-                            suma21 += Integer.parseInt( String.valueOf( board.table.getValueAt( i, 2 ) ) );
-                        } else if (7 < i && i < 15) {
-                            suma22 += Integer.parseInt( String.valueOf( board.table.getValueAt( i, 2 ) ) );
-                        }
-                        a = Integer.parseInt( String.valueOf( board.table.getValueAt( i, 2 ) ) );
-                        b = i;
-                    }
-                    pr.println(6 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 6, 2))));
-                    pr.println( 7 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 7, 2 ) ) ) );
-                    pr.println( 15 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 15, 2 ) ) ) );
-                    pr.println( 16 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 16, 2 ) ) ) );
+                board.text.setText("Tura gracza: " + "\n" + player2Name + "\nPozostało \nrzutów: " + 3);
 
+                for (int i = 0; i < 17; i++) {
+                        board.table.setValueAt(suma11, 7, 1);
+                        board.table.setValueAt(suma12, 15, 1);
+                        if(suma11 >= 63){
+                            board.table.setValueAt("35", 6, 1);
+                        }
+                        razem = suma11 + suma12 + Integer.parseInt( String.valueOf( board.table.getValueAt( 6, 1 ) ));
+                        board.table.setValueAt(razem,16,1);
+                        if (board.table.isRowSelected( i ) == true) {
+                            if (i < 7) {
+                                suma11 += Integer.parseInt( String.valueOf( board.table.getValueAt( i, 1 ) ) );
+                            } else if (7 < i && i < 15) {
+                                suma12 += Integer.parseInt( String.valueOf( board.table.getValueAt( i, 1 ) ) );
+                            }
+                            a = Integer.parseInt( String.valueOf( board.table.getValueAt( i, 1 ) ) );
+                            b = i;
+                        }
+                        pr.println(6 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 6, 1))));
+                        pr.println( 7 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 7, 1 ) ) ) );
+                        pr.println( 15 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 15, 1 ) ) ) );
+                        pr.println( 16 + "," + Integer.parseInt( String.valueOf( board.table.getValueAt( 16, 1 ) ) ) );
                 }
                 pr.println(b + "," + a);
                 turn = false;
                 int code = 11;
                 pr.println( code );
                 rolls = 3;
-                if(gameLength > 12)
-                    game = false;
+                System.out.println( gameLength );
                 pr.println( code );
                 pr.flush();
             }
-            }
-            );
+        } );
         while(game) {
             String str;
             int[] message;
@@ -263,7 +262,7 @@ public class Main {
             while (!turn) {
                 try {
                     str = bf.readLine();
-                    System.out.println( "otrzymano: " + str );
+                    //System.out.println( "otrzymano: " + str );
                     message = parseSocketMsg( str );
                     if (message.length == 5) {
                         dice1 = getDice( message[0] );
@@ -282,24 +281,41 @@ public class Main {
                     }
 
                     if (message[0] == 11) {
+                        gameLength +=1;
                         turn = true;
                         rolls = 3;
-                        board.text.setText("Tura gracza: " + "\n" + player2Name + "\nPozostało \nrzutów: " + rolls);
-                        System.out.println(turn);
+                        board.text.setText("Tura gracza: " + "\n" + player1Name + "\nPozostało \nrzutów: " + rolls);
+                        if(gameLength > 12) {
+                            System.out.println( "Koniec gry" );
+                            game = false;
+                            int suma1 = Integer.parseInt( String.valueOf( board.table.getValueAt( 16, 1 )));
+                            int suma2 = Integer.parseInt( String.valueOf( board.table.getValueAt( 16, 2 )));
+                            if(suma1>suma2) {
+                                String wygrany = player1Name;
+                                JOptionPane.showConfirmDialog(null,"Koniec rozgrywki, wygrał/a:" + player1Name, "Tytuł okienka", JOptionPane.CANCEL_OPTION);
+                            }
+                            if(suma1 < suma2){
+                                String wygrany = player2Name;
+                                JOptionPane.showConfirmDialog(null,"Koniec rozgrywki, wygrał:" + player2Name,"Tytuł okienka", JOptionPane.CANCEL_OPTION);
+                            }
+                            if(suma1 == suma2) {
+                                String wygrany = "remis";
+                                JOptionPane.showConfirmDialog(null,"Koniec rozgrywki, remis","Tytuł okienka", JOptionPane.CANCEL_OPTION);
+                            }
+                        }
                     }
                     if(message.length == 2) {
-                        board.table.setValueAt( message[1], message[0], 1 );
+                        board.table.setValueAt( message[1], message[0], 2 );
                     }
-
-                    System.out.println( message );
+                    //System.out.println( message );
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
             }
         }
-}
 
-    private static Image getZaz(int kostka) {
+}
+private static Image getZaz(int kostka) {
         Image zaz;
         switch (kostka) {
             case 1:
